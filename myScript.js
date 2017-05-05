8504bbe25c7b53dd15f0751758ae8c6dfd5178b6
Lee <leebee1234@hotmail.co.uk>
2017-05-05 21:40:05 +0100

    added

---
+++ myScript.js
@@ -0,0 +1,28 @@
+﻿let options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+const output = document.getElementById("myLocation");
+
+function success(pos) {
+	
+	let latitude  = pos.coords.latitude;
+                        let longitude = pos.coords.longitude;
+	let accuracy = pos.coords.accuracy;
+
+    output.innerHTML = `Latitude is ${latitude} and Longitude is ${longitude}. More or less ${accuracy} metres.`;
+
+    const img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+    output.appendChild(img);
+
+};
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
