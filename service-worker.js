70e24c8d0776f3ff59f40c6b32df969b20481ce9
Lee <leebee1234@hotmail.co.uk>
2017-05-06 00:28:32 +0100

    added

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+              <h3>Form</h3>
+            <form id="CornwallForm">
+              <p>
+                <label for="name">Name: </label>
+                <input name="name" />
+              </p>
+
+              <p>
+                <label for="email">Email: </label>
+                <input name="email" type="email" />
+              </p>
+
+              <p>
+                <label for="question">Question: </label>
+                <input name="question" />
+              </p>
+
+              <p>
+                <input type="submit" value="Ask" />
+              </p>
+            </form>
